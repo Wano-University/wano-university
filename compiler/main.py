72317889c72f6185ef617a206bd649361f156e2e
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2026-05-25 21:48:16 +0100

    some changes to the lexer and parser to accept both languages + some reserved words

--- compiler/main.py
+++ compiler/main.py
@@ -1,59 +1,94 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
-t_ignore = ' \t'
-
-
-variables  = dict()
+t_ignore = " \t"
 
 reserved = {
-        'if':'IF',
-        'then':'THEN',
-        'else':'ELSE',
-        'reservar':'RESERVAR',
-        }
+    "if": "IF",
+    "then": "THEN",
+    "else": "ELSE",
+    "comprar": "ACTION",
+    "buy": "ACTION",
+    "registar": "ACTION",
+    "register": "ACTION",
+    "reservar": "ACTION",
+    "reserve": "ACTION",
+    "cancelar": "ACTION",
+    "cancel": "ACTION",
+    "sala": "ROOM",
+    "room": "ROOM",
+    "equipamento": "EQUIPMENT",
+    "equipment": "EQUIPMENT",
+    "sensor": "SENSOR",
+    "laboratorio": "LAB",
+    "laboratory": "LAB",
+}
 
-tokens=[
-        'ID',
-        'TYPE',
-        'DATA',
-        ] + list(reserved.values())
+tokens = [
+    "ID",
+    "TIME",
+    "COLON",
+    "COMMA",
+] + list(set(reserved.values()))
 
 
-
-def t_TIPO(t):
-    r'sala|equipamento|sensor|laboratorio'
+def t_TIME(t):
+    r"([01][0-9]|2[0-3]):[03]0"
     return t
 
-def t_ACTION(t):
-    r'[cC]omprar|[rR]egistar|[rR]eservar '
-
-    return t
-
-def t_HORA(t):
-    r'([01][0-9]|2[0-3]):[03]0'
-    return t
 
 def t_ID(t):
-
-    r'[a-zA-Z]+[0-9]*'
-    t.type = reserved.get(t.value,'ID')
+    r"[a-zA-Z]+[0-9]*"
+    t.type = reserved.get(t.value.lower(), "ID")
     return t
+
 
 def t_COLON(t):
-    r':'
+    r":"
     return t
+
+
 def t_COMMA(t):
-    r','
+    r","
     return t
+
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+
+def p_type(p):
+    """type : ROOM
+    | EQUIPMENT
+    | SENSOR
+    | LAB"""
+    p[0] = p[1]
+
 
 def p_action(p):
-    '''registo: ACTION objects  '''
+    """registration : ACTION objects"""
+    pass
+
 
 def p_objects(p):
-    '''TIPO COLON ID COMMA HORA '''
+    """objects : type COLON ID COMMA TIME"""
+    pass
 
-def p_reserva()
 
-def p_cancelamento()
+def p_reservation(p):
+    pass
 
+
+def p_cancellation(p):
+    pass
+
+
+def p_error(p):
+    print("Syntax error!")
+
+
+parser = yacc.yacc()
