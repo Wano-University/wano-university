030260520102ede3326ae9366f1d9368478cebd8
Falconz0012 <tomasfalcao.26@gmail.com>
2026-06-06 21:56:03 +0000

    Trying to fix pr

--- backend/src/routes/TicketRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTicketsByUser, updateTicketStatus } from '../controllers/TicketController.js';
-
-const router = Router();
-
-router.post('/user', getTicketsByUser);
-router.patch('/status', updateTicketStatus);
-
-export default router;
