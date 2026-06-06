5f0c517fd8aad6881f260cda3796ed3011f33973
Falconz0012 <tomasfalcao.26@gmail.com>
2026-06-06 22:10:58 +0000

    is this it ?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,14 +17,8 @@ import ForgotPassword from './pages/ForgotPassword';
 import ChangePassword from './pages/ChangePassword';
 import MyTickets from './pages/MyTickets';
 import ValidateTicket from './pages/ValidateTicket';
-import MyReservations from './pages/MyReservations';
-import TemperatureDashboard from './pages/TemperatureDashboard';
-import AirQualityDashboard from './pages/AirQualityDashboard';
-import EnergyConsumptionDashboard from './pages/EnergyConsumptionDashboard';
+import LssDocs from './pages/Lssdocs';
 import { TerminalProvider, TerminalPopup } from './components/TerminalPopup';
-import NotFound from './components/NotFound';
-import Footer from './components/Footer';
-import ValidateReservation from './pages/ValidateReservation';
 
 function RootLayout() {
   return (
@@ -36,7 +30,6 @@ function RootLayout() {
           <main className="flex-1">
             <Outlet />
           </main>
-          <Footer />
         </div>
       </TerminalProvider>
     </ThemeProvider>
@@ -50,6 +43,7 @@ export default function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/createacc" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/lssdocs" element={<LssDocs/>} />
         <Route path="/changepw" element={<ForgotPassword />} />
         <Route path="/spaces" element={<Spaces />} />
         <Route path="/home" element={<HomeRedirection />} />
@@ -59,18 +53,10 @@ export default function App() {
         <Route path="/admin" element={<ProtectedRoute allowedRoles={['ADMIN']}> <AdminHome /> </ProtectedRoute>} />
         <Route path="/staff" element={<ProtectedRoute allowedRoles={['STAFF']}> <StaffHome /> </ProtectedRoute>} />
         <Route path="/validate-ticket/:id" element={<ProtectedRoute allowedRoles={['STAFF']}> <ValidateTicket /> </ProtectedRoute>} />
-        <Route path="/validate-reservation/:id" element={<ProtectedRoute allowedRoles={['STAFF']}> <ValidateReservation /> </ProtectedRoute>} />
-        <Route path="/student" element={<ProtectedRoute allowedRoles={['STUDENT', 'TEACHER']}> <StudentHome /> </ProtectedRoute>} />
-        <Route path="/reservations" element={<ProtectedRoute allowedRoles={['STUDENT', 'TEACHER']}> <MyReservations /> </ProtectedRoute>} />
-
+        <Route path="/student" element={<ProtectedRoute allowedRoles={['STUDENT']}> <StudentHome /> </ProtectedRoute>} />
+        <Route path="/student" element={<ProtectedRoute allowedRoles={['TEACHER']}> <StudentHome /> </ProtectedRoute>} />
         <Route path="/map" element={<ProtectedRoute allowedRoles={['ADMIN', 'STAFF']}> <InteractiveMap /> </ProtectedRoute>} />
         <Route path="/menuconfig" element={<ProtectedRoute allowedRoles={['ADMIN', 'STAFF']}> <MenuConfig /> </ProtectedRoute>} />
-
-        <Route path="/TemperatureDashboard" element={<ProtectedRoute allowedRoles={['ADMIN', 'STAFF']}> <TemperatureDashboard /> </ProtectedRoute>} />
-        <Route path="/AirQualityDashboard" element={<ProtectedRoute allowedRoles={['ADMIN', 'STAFF']}> <AirQualityDashboard /> </ProtectedRoute>} />
-        <Route path="/EnergyConsumptionDashboard" element={<ProtectedRoute allowedRoles={['ADMIN', 'STAFF']}> <EnergyConsumptionDashboard /> </ProtectedRoute>} />
-
-        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
